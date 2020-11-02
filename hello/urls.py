466ce898a46962aaaf3f5078bd5565b7d0edd1ea
alial95 <alistair_j_cooper@hotmail.com>
2020-11-02 14:26:23 +0000

    added new page

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from django.contrib import admin
 from . import views
 urlpatterns = [
-    # path()
+    path('new_entry/', views.new_entry, name='new_entry')
 ]
