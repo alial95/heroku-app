6f22eecb2d5aa3444b7bb1162c0f4c987e818299
alial95 <alistair_j_cooper@hotmail.com>
2020-11-23 12:15:24 +0000

    migrated postgres database

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -80,9 +81,10 @@ DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        
     }
 }
-
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
