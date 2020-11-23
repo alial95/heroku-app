4fed9e3a47db615443501b539a8a4196e3e9e6b1
alial95 <alistair_j_cooper@hotmail.com>
2020-11-23 20:15:17 +0000

    djdjd

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+# DATABASES = {
+#     "default": {
+#         "ENGINE" : "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
         
-    }
-}
+#     }
+# }
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
