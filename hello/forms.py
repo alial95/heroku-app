402395be0e0fbaa82760710da352b3170b7d0e86
alial95 <alistair_j_cooper@hotmail.com>
2020-11-23 10:50:34 +0000

    added new drink form

--- hello/forms.py
+++ hello/forms.py
@@ -4,5 +4,5 @@ from hello.models import Drinks
 class DrinkForm(forms.ModelForm): 
     class Meta:
         model = Drinks
-        fields = ['drink name']
-        labels = {'drink': ''}
+        fields = ['name']
+        labels = {'name': ''}
