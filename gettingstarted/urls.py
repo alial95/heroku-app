cde12833177aab3465e5a012da8dbc6bd13ba47c
alial95 <alistair_j_cooper@hotmail.com>
2020-11-23 13:28:20 +0000

    added drink to database

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', hello.views.projects, name='projects'),
     path('', include('hello.urls'), name='hello'),
     path('drinks/', hello.views.drinks, name='drinks'),
-    path('new_drinks/', hello.views.add_drink, name='new_drink')
+    path('newdrinks/', hello.views.add_drink, name='new_drink')
 ]
