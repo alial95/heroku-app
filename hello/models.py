aa8f63f2608f58dcbc1ee2e77ddc76994717f94a
alial95 <alistair_j_cooper@hotmail.com>
2020-11-23 19:06:13 +0000

    changed typo in person class

--- hello/models.py
+++ hello/models.py
@@ -35,7 +35,7 @@ class Sizes(models.Model):
 
 class Drinks(models.Model):
     name = models.CharField(max_length=15)
-    size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
+    size = models.ForeignKey(Sizes, default=1, on_delete=models.CASCADE)
     price = models.FloatField()
     def __str__(self):
         return f"Drink Name: {self.name}. Price: {self.price}. Size: {self.size}"
@@ -43,7 +43,7 @@ class Drinks(models.Model):
 class Person(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
-    favourite_drink = models.ForeignKey(Drinks, on_delete=models.CASCADE)
+    favourite_drink = models.ForeignKey(Drinks, default=1, on_delete=models.CASCADE)
     age = models.IntegerField()
 
     def __str__(self):
