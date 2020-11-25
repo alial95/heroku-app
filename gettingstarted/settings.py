6976b17a23a0925b68b5e78a895ffdfde8b472ad
alial95 <alistair_j_cooper@hotmail.com>
2020-11-25 15:18:26 +0000

    changed location of register template

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -112,8 +112,8 @@ USE_L10N = True
 
 USE_TZ = True
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'drinks'
-LOGOUT_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
