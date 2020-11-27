e95e24790f67dbda27cef3feaf10ea5e9d4a7062
alial95 <alistair_j_cooper@hotmail.com>
2020-11-27 12:47:24 +0000

    fixing the url paths

--- hello/urls.py
+++ hello/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from . import views
 app_name = 'hello'
 urlpatterns = [
-    path('new_entry.html', views.new_entry, name='new_entry'),
-    path('projects.html', views.projects, name='projects'),
+    path('', views.drinks, name='drinks'),
+    # path('projects.html', views.projects, name='projects'),
     # path('drinks.html', views.drinks, name='drinks')
     
 ]
